00db057693c88e0603b9ac31158fd7aba0656968
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-08 20:08:45 -0500

    Updated JS on game stats to magnify on hover

--- public/js/application.js
+++ public/js/application.js
@@ -11,22 +11,36 @@
 
 
 $(document).ready(function() {
+  
+  // magnifies links on hover
+  $('#history a').hover(function(){
+    $(this).css({"font-size" : "20px"})
+  },
+  function(){
+    $(this).css({"font-size" : "15px"})
+  }); 
+
+  // This opens and closes the game stats on the user page
   $("#history a").on("click", function(event){
     event.preventDefault()
     var url = $(this).attr("href")
 
     if($(this).hasClass('clicked')==false){
+      $("a").removeClass("clicked")
       $(this).addClass("clicked")
       $.get(url, function(response){
-        $(".container").append(response)
+        $("#appended_stats").remove()
+        $('.clicked').next().append(response)
       });
-
+ 
     } else {
       $("#appended_stats").remove()
       $(this).removeClass("clicked")
     }
 
   });
+
+  
 });
 
 
